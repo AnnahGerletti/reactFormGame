559d56551c6271e9746bfbf3c36ce4d573498cba
sherlyc <sherlyc@gmail.com>
2017-07-31 17:58:39 +1200

    added some notes to simpleForm

--- client/components/SimpleForm.jsx
+++ client/components/SimpleForm.jsx
@@ -21,3 +21,6 @@ export default class SimpleForm extends React.Component {
   }
 
 }
+//figure out how to create two forms after player one hits submit the answers populate the object and the the simpleform is rendered again. Or have two forms with the first submit the form2 can be rendered. Then after the submit is clicked twice the results apear on a new page.
+//make simple form like new form, get it to populate the obj, send this.state.answers when submit is hit.
+//could also pull out answers and put them into thier own obj. 
